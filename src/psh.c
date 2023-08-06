e068f8a6e8f3bec83ecfa17f30d1f5f21f9241a5
Jozef Nagy <schkwve@gmail.com>
2023-08-06 12:20:41 +0200

    Improved error handling

--- src/psh.c
+++ src/psh.c
@@ -38,12 +38,18 @@ int main()
 
 	for (;;) {
 		printf("%s ", prompt);
+
 		int num_bytes = getline(&_g_buffer, &buffer_size, stdin);
+		// EOF
 		if (num_bytes == -1) {
-			perror(strerror(errno));
-			exit(1);
+			exit(0);
 		} else if (num_bytes == 0) {
 			continue;
+		}
+
+		// check for C-D / NULL
+		if (_g_buffer == NULL) {
+			exit(0);
 		}
 
 		command_parse(_g_buffer);
